e30f962ee6bb48bdaa08aee49d51c73bc149d2cd
stevenzack <zigzigcheers@gmail.com>
2021-05-30 09:27:15 +0800

    api-dir

--- api/gogen/gen.go
+++ api/gogen/gen.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"io/fs"
+	"log"
 	"os"
 	"os/exec"
 	"path"
@@ -16,6 +18,7 @@ import (
 	"github.com/gofaith/go-zero/core/logx"
 	apiformat "github.com/gofaith/goctlr/api/format"
 	"github.com/gofaith/goctlr/api/parser"
+	"github.com/gofaith/goctlr/api/spec"
 	apiutil "github.com/gofaith/goctlr/api/util"
 	"github.com/gofaith/goctlr/util"
 	"github.com/logrusorgru/aurora"
@@ -40,29 +43,102 @@ func GoCommand(c *cli.Context) error {
 		logx.Must(genProto(dir, proto))
 	}
 
-	p, err := parser.NewParser(apiFile)
-	if err != nil {
-		return err
-	}
-	api, err := p.Parse()
-	if err != nil {
-		return err
+	info, e := os.Stat(apiFile)
+	if e != nil {
+		log.Println(e)
+		return e
 	}
 
-	logx.Must(util.MkdirIfNotExist(dir))
-	logx.Must(genEtc(dir, api))
-	logx.Must(genConfig(dir))
-	logx.Must(genMain(dir, api))
-	logx.Must(genServiceContext(dir, api))
-	if len(proto) == 0 {
-		logx.Must(genTypes(dir, api))
-	}
-	logx.Must(genHandlers(dir, proto, api))
-	logx.Must(genRoutes(dir, api))
-	logx.Must(genLogic(dir, proto, api))
-	if c.Bool("clitest") {
-		logx.Must(genClient(dir, api))
-		logx.Must(genTest(dir, api))
+	if info.IsDir() {
+
+		//check
+		typeMap := make(map[string]string)
+		routeMap := make(map[string]string)
+		apiList := []*spec.ApiSpec{}
+		e = filepath.Walk(apiFile, func(path string, info fs.FileInfo, err error) error {
+			if info.IsDir() || !strings.HasSuffix(path, ".api") {
+				return nil
+			}
+
+			p, e := parser.NewParser(path)
+			if e != nil {
+				log.Println(path + ":" + e.Error())
+				return e
+			}
+			api, e := p.Parse()
+			if e != nil {
+				log.Println(path + ":" + e.Error())
+				return e
+			}
+			//type check
+			for _, typ := range api.Types {
+				if before, ok := typeMap[typ.Name]; ok {
+					return errors.New(path + ": type name duplicated \"" + typ.Name + "\", between file \"" + filepath.Base(path) + "\" and \"" + filepath.Base(before) + "\"")
+				}
+				typeMap[typ.Name] = path
+			}
+			//route check
+			for _, route := range api.Service.Routes {
+				if before, ok := routeMap[route.Path]; ok {
+					return errors.New(path + ": route path duplicated \"" + route.Path + "\", between file \"" + filepath.Base(path) + "\" and \"" + filepath.Base(before) + "\"")
+				}
+				routeMap[route.Path] = path
+			}
+
+			apiList = append(apiList, api)
+			return nil
+		})
+		if e != nil {
+			log.Println(e)
+			return e
+		}
+
+		//generate
+		for _, api := range apiList {
+			logx.Must(util.MkdirIfNotExist(dir))
+			logx.Must(genEtc(dir, api))
+			logx.Must(genConfig(dir))
+			logx.Must(genServiceContext(dir, api))
+			if len(proto) == 0 {
+				logx.Must(genTypes(dir, api))
+			}
+			logx.Must(genHandlers(dir, proto, api))
+			logx.Must(genRoutes(dir, api))
+			logx.Must(genLogic(dir, proto, api))
+			if c.Bool("clitest") {
+				logx.Must(genClient(dir, api))
+				logx.Must(genTest(dir, api))
+			}
+			api.Service.Name = "application"
+			logx.Must(genMain(dir, api))
+		}
+	} else {
+		p, e := parser.NewParser(apiFile)
+		if e != nil {
+			log.Println(apiFile + ":" + e.Error())
+			return e
+		}
+		api, e := p.Parse()
+		if e != nil {
+			log.Println(apiFile + ":" + e.Error())
+			return e
+		}
+
+		logx.Must(util.MkdirIfNotExist(dir))
+		logx.Must(genEtc(dir, api))
+		logx.Must(genConfig(dir))
+		logx.Must(genMain(dir, api))
+		logx.Must(genServiceContext(dir, api))
+		if len(proto) == 0 {
+			logx.Must(genTypes(dir, api))
+		}
+		logx.Must(genHandlers(dir, proto, api))
+		logx.Must(genRoutes(dir, api))
+		logx.Must(genLogic(dir, proto, api))
+		if c.Bool("clitest") {
+			logx.Must(genClient(dir, api))
+			logx.Must(genTest(dir, api))
+		}
 	}
 	// it does not work
 	format(dir)
@@ -72,7 +148,7 @@ func GoCommand(c *cli.Context) error {
 		return err
 	}
 
-	if err = apiformat.ApiFormat(apiFile, false); err != nil {
+	if err := apiformat.ApiFormat(apiFile, false); err != nil {
 		return err
 	}
 
